5548ea4416b1cb14df98d3fc910afda5074d50e2
parhamsamee <hallsamee@gmail.com>
2021-03-04 12:05:55 -0600

    12.6.4. Processing Form Submission

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,7 +1,11 @@
 package org.launchcode.spaday.controllers;
 
+import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,6 +14,20 @@ public class UserController {
 
     @GetMapping("add")
     public String displayAddUserForm() {
-        return "/user/add";
+        return "user/add";
+    }
+
+    @PostMapping()
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        //validate password match
+        if(verify.equals(user.getPassword())) {
+            //is true
+            //view template with a message that welcomes the user by username
+            model.addAttribute("user", user);
+            return "user/index";
+        } else {
+            // is false
+            return "user/add";
+        }
     }
 }
