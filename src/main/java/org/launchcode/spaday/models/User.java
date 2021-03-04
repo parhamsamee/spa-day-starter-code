a4dd2520bff1730ffb85f707f70a41717ca4f66a
parhamsamee <hallsamee@gmail.com>
2021-03-04 09:11:23 -0600

    12.6.2. Creating the Model

---
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -0,0 +1,24 @@
+package org.launchcode.spaday.models;
+
+public class User {
+    //Variables username, email, password
+    private String username;
+    private String email;
+    private String password;
+
+    //Constructors
+
+    //Getters && Setters
+
+    public String getUsername() { return username; }
+
+    public void setUsername(String username) { this.username = username; }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+
+    public void setPassword(String password) { this.password = password; }
+}
